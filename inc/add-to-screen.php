f02ed97bb23023ad808225392d4a3835a6162947
tang-edgetech <tang.edgetech@gmail.com>
2025-06-09 16:36:19 +0800

    Update directory

---
+++ inc/add-to-screen.php
@@ -0,0 +1,25 @@
+<section class="add-to-screen" id="">
+    <div class="container-fluid">
+        <div class="row justify-content-center">
+            <div class="col-12 col-md-11 col-xl-9 col-xxl-7 px-4">
+                <div id="a2hs-banner" class="default">
+                    <div class="d-flex flex-column flex-md-row justify-content-center align-items-center">
+                        <div class="col-12 col-md-9 text-center text-md-start pe-md-4">
+                            <h4>Add to Home Screen</h4>
+                            <p class="mb-4 mb-md-0">Add this app to your home screen for a better experience.</p>
+                        </div>
+                        <div class="col-12 col-md-3 ps-md-4">
+                            <div class="btn-wrapper justify-content-center justify-content-md-start">
+                                <button id="install-btn" class="btn btn-default">
+                                    <span class="d-none">Add</span>
+                                    <i class="fa fa-home"></i>
+                                    <span class="floating-plus"><i class="fa fa-plus-circle"></i></span>
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
---
+++ inc/quick-enquiry.php
@@ -0,0 +1,13 @@
+<section class="enquiry-sources" id="">
+    <div class="container-fluid">
+        <div class="row justify-content-center">
+            <div class="col-12 col-md-11 col-xl-6 col-xxl-5 px-4">
+                <div class="page-starter d-block mx-auto mb-4"><img src="./images/mega888-logo.png" class="img-fluid w-100"/></div>
+                <div class="btn-wrapper d-flex flex-row flex-nowrap justify-content-center">
+                    <a href="#" class="btn btn-rounded btn-with-icon btn-telegram text-white"><?php echo $icon_telegram_white;?><span>Telegram</span></a>
+                    <a href="#" class="btn btn-rounded btn-with-icon btn-whatsapp text-white"><?php echo $icon_whatsapp_white;?><span>WhatsApp</span></a>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
