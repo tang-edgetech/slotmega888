8322dc769733ae617cf1940bce65faae989f3383
tang-edgetech <tang.edgetech@gmail.com>
2025-07-11 16:06:33 +0800

    Update download sources linkages

--- inc/quick-enquiry.php
+++ inc/quick-enquiry.php
@@ -35,7 +35,7 @@
                                 <div class="platform-content text-start">
                                     <h4 class="text-white fw-normal mb-4"><b>Android</b></h4>
                                     <div class="btn-wrapper justify-content-center">
-                                        <a href="#" class="btn btn-rounded btn-with-icon btn-download text-white" target="_blank" title="Android"><i class="fa-solid fa-download" aria-hidden="true"></i><span>Download</span></a>
+                                        <a href="https://bit.ly/mg888apk1q" rel="nofollow" class="btn btn-rounded btn-with-icon btn-download text-white" target="_blank" title="Android"><i class="fa-solid fa-download" aria-hidden="true"></i><span>Download</span></a>
                                     </div>
                                 </div>
                             </div>
@@ -51,7 +51,7 @@
                                 <div class="platform-content text-start">
                                     <h4 class="text-white fw-normal mb-4"><b>iOS</b></h4>
                                     <div class="btn-wrapper justify-content-center">
-                                        <a href="#" class="btn btn-rounded btn-with-icon btn-download text-white" target="_blank" title="iOS"><i class="fa-solid fa-download" aria-hidden="true"></i><span>Download</span></a>
+                                        <a href="https://appsetup.zjhenghong.com/ios/plist/Mega888_0.plist" rel="nofollow" class="btn btn-rounded btn-with-icon btn-download text-white" target="_blank" title="iOS"><i class="fa-solid fa-download" aria-hidden="true"></i><span>Download</span></a>
                                     </div>
                                 </div>
                             </div>
@@ -67,7 +67,7 @@
                                 <div class="platform-content text-start">
                                     <h4 class="text-white fw-normal mb-4"><b>Windows</b></h4>
                                     <div class="btn-wrapper justify-content-center">
-                                        <a href="#" class="btn btn-rounded btn-with-icon btn-download text-white" target="_blank" title="Windows"><i class="fa-solid fa-download" aria-hidden="true"></i><span>Download</span></a>
+                                        <a href="https://bit.ly/mgpc1q" rel="nofollow" class="btn btn-rounded btn-with-icon btn-download text-white" target="_blank" title="Windows"><i class="fa-solid fa-download" aria-hidden="true"></i><span>Download</span></a>
                                     </div>
                                 </div>
                             </div>
