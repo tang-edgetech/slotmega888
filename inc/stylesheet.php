7c990cd5d47f3b249449a06832702e45f32c5e68
weiyee <edgetechnology456@gmail.com>
2025-07-22 11:25:21 +0800

    add google tag

--- inc/stylesheet.php
+++ inc/stylesheet.php
@@ -5,9 +5,18 @@
 <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossorigin="anonymous">
 <link rel="stylesheet" type="text/css" href="./css/swiper-bundle.min.css"/>
-<!-- <link rel="stylesheet" type="text/css" href="css/"/> -->
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-<!-- <link rel="stylesheet" type="text/css" href="./css/font-awesome.min.css"/> -->
+
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-B8BZNYE00C"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-B8BZNYE00C');
+</script>
+
 <?php
 if( !empty($page_slug) && $page_slug == 'home' ) {
     echo '<link rel="stylesheet" type="text/css" href="./css/slotmachine.css"/>';
