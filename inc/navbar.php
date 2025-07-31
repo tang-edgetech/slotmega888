103a0a256d8634dcd340f2f37006419d1076aa2d
weiyee <edgetechnology456@gmail.com>
2025-07-31 17:23:01 +0800

    testing change url to cdn url

--- inc/navbar.php
+++ inc/navbar.php
@@ -6,7 +6,7 @@
                 <picture>
                     <source srcset="./images/mega888-logo.webp" type="image/webp">
                     <source srcset="./images/mega888-logo.png" type="image/png">
-                    <img src="<?php echo $site_base_url;?>images/mega888-logo.png" width="250px" height="250px" alt="Mega888 Logo" class="img-fit">
+                    <img src="https://88mega88slot.b-cdn.net/images/mega888-logo.png" width="250px" height="250px" alt="Mega888 Logo" class="img-fit">
                 </picture>
             </a>
             <button type="button" class="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#main-navigation" aria-controls="main-navigation" aria-expanded="false" aria-label="Toggle navigation">
