cb6bbc2800082b6490a2dd9d06bc79cc0e38de5a
tang-edgetech <tang.edgetech@gmail.com>
2025-08-06 17:32:23 +0800

    Update Accordion to the new 3 blogs

--- how-to-download-install-mega888-apk/index.php
+++ how-to-download-install-mega888-apk/index.php
@@ -8,6 +8,7 @@ $page_slug = $post['slug'];
 $page_thumbnail = "https://example.com/image.jpg";
 $meta_tags = $post['meta_tags'];
 $markup_schema = $post['schema'];
+$faq = $post['faq'];
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -55,6 +56,39 @@ $markup_schema = $post['schema'];
                     </div>
                 </div>
             </section>
+            
+            <?php if( $faq ) { ?>
+            <section class="" id="faq">
+                <div class="container-fluid">
+                    <div class="row justify-content-center">
+                        <div class="col-12 col-md-11 col-xl-9 col-xxl-7 px-0 px-md-0">
+                            <h2>Soalan Lazim (FAQ)</h2>
+                            <div class="accordion" id="accordionFAQ">
+                            <?php $i = 1;
+                            foreach ( $faq as $item ) {
+                                $title = $item['heading'];
+                                $body = $item['body'];
+                                if( $title && $body ) {
+                                    $heading_class = 'accordion-button';
+                                    $heading_expanded = 'true';
+                                    $body_class = 'accordion-collapse collapse show';
+                                    if( $i > 1 ) {
+                                        $heading_class = 'accordion-button collapsed';
+                                        $heading_expanded = 'false';
+                                        $body_class = 'accordion-collapse collapse';
+                                    }
+                                    $index = str_pad($i, 2, "0", STR_PAD_LEFT);
+                                    $body = convert_site_base_url($body);
+                                    echo '<div class="accordion-item"><h3 class="accordion-header"><button class="'.$heading_class.'" type="button" data-bs-toggle="collapse" data-bs-target="#collapse'.$index.'" aria-expanded="'.$heading_expanded.'" aria-controls="collapse'.$index.'">'.$title.'</button></h3><div id="collapse'.$index.'" class="'.$body_class.'" data-bs-parent="#accordionFAQ"><div class="accordion-body">'.$body.'</div></div></div>';
+                                    $i++;
+                                }
+                            } ?>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <?php } ?>
         </main>
         <?php include '../inc/footer.php';?>
         <?php include '../inc/popup-share-this.php';?>
